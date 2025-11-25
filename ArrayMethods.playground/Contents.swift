83384a02806eecf1c5ae76295a1a473bd9ee66e6
ShreyasTantri <shreyastantrik29@gmail.com>
2025-11-25 22:18:16 +0530

    Explored basic Array methods that included Adding, Inserting and Removing Array items

--- ArrayMethods.playground/Contents.swift
+++ ArrayMethods.playground/Contents.swift
@@ -1,3 +1,30 @@
-import UIKit
+// Array Methods
+var numbers = [1,2,3]
 
-var greeting = "Hello, playground"
+// 1. Append
+numbers.append(4)
+print(numbers)  // [1,2,3,4]
+
+// 2. Insert
+numbers.insert(100, at: 0)
+print(numbers)  // [100,1,2,3,4]
+
+// 3. Remove
+numbers.remove(at: 4)
+print(numbers)  // [100,1,2,3]
+
+// 4. Remove last - Alternative popLast()
+numbers.removeLast()
+print(numbers)  // [100,1,2]
+
+// 5. Remove All
+numbers.removeAll()
+print(numbers)  // []
+
+
+var words = ["red", "blue", "green", "blue"]
+
+words.removeAll{ item in
+    item == "blue"
+}
+print(words)
