84dbb5d20fb0df73be19fcaa2115942d1586b68b
ShreyasTantri <shreyastantrik29@gmail.com>
2025-11-29 12:34:07 +0530

    Updated Higher order functions and added Array slicing methods

--- ArrayMethods.playground/Contents.swift
+++ ArrayMethods.playground/Contents.swift
@@ -47,16 +47,14 @@ if let idx = fruits.firstIndex(of: "Orange") {
 }
 
 // 3. isEmpty
-
 var items: [Int] = []
 print(items.isEmpty)
 
-let b = [1,2,3]
-print(b.isEmpty)
+let items2 = [1,2,3]
+print(items.isEmpty)
 
 
 //  *** Sorting and Reordering ***
-
 // 1. Sorted
 var ranks = [3,66,123,453,4,7]
 var sortedRanks = ranks.sorted()
@@ -91,10 +89,9 @@ var num4 = [3,66,123,453,4,7]
 num4.reverse()
 
 //  *** Higher order functions ***
+// *** map, filter, reduce, flatmap, compactmap, foreach ***
 
-// *** map, filter, reduce, flatmap, contact map, foreach ***
-
-// 1. map
+// 1. map (transforms values)
 var digits = [1,4,6,8]
 var newdigits1 = digits.map({ item in
  item + 10
@@ -108,7 +105,12 @@ var examples = [1,4,5,2,7]
 var newexamples = examples.map({ [$0, $0 * 4] })
 print(newexamples)
 
-// 2. filter
+// 2. flatmap (transforms values and removes any nested array)
+var copies = [1,4,5,2,7]
+var newcopies = copies.flatMap({ [$0, $0 * 4] })
+print(newcopies)
+
+// 3. filter
 var sample = [1,4,6,8]
 var newsample = sample.filter({ $0 % 2 == 0 })
 print(newsample)
@@ -117,13 +119,39 @@ var names = ["Shreyas", "Swathi", "Raj", "Jim"]
 var newnames = names.filter({ $0.count > 4 })
 print(newnames)
 
-// 3. reduce
+// 4. reduce
+var values = [1,2,3,4]
+values.reduce(0) { $0 + $1 }
 
-// 4. flatmap
-var copies = [1,4,5,2,7]
-var newcopies = copies.flatMap({ [$0, $0 * 4] })
-print(newcopies)
+// 5. compactmap (transforms and removes nil)
+var age = ["12","13","adr"]
+print("compact map")
 
-// 5. foreach
+let newAgeMapped = age.map({ Int($0) })
+print(newAgeMapped)
+let newAgeCompactmapped = age.compactMap({ Int($0) })
+print(newAgeCompactmapped)
 
-// *** Slicing
+// 6. forEach
+var sampleValues = [1,3,4,8]
+sampleValues.forEach({ print($0 * 2) })
+print("******")
+var sampleValues1 = [1,3,4,8]
+sampleValues1.forEach({ item in
+        print(item * 3)
+})
+
+// *** Slicing ***
+var num5 = [3,66,123,453,4,7]
+
+// 1. prefix
+print(num5.prefix(3))
+
+// 2. Suffix
+print(num5.suffix(2))
+
+// 3. dropLast
+print(num5.dropLast(2))
+
+// 4. dropFirst
+print(num5.dropFirst(5))
